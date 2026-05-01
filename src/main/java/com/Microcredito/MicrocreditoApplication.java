5f771bc50a4e91e106e10e1939e0a9f0c2f818b6
Yasser Boaventura <yasserboaventura78@gmail.com>
2026-05-01 09:59:50 +0100

    started!

---
+++ src/main/java/com/Microcredito/MicrocreditoApplication.java
@@ -0,0 +1,13 @@
+package com.Microcredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrocreditoApplication.class, args);
+	}
+
+}
